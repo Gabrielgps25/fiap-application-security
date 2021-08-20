74be6ef7bf0be14107ae35a0f7739bdbdde40e86
Gabriel Pereira <gabrielgps2510@gmail.com>
2021-08-19 23:44:40 -0300

    fix: Implements RateLimit to avoid BrutalForce

--- lab2-nodejs-broken-autentication/application/app.js
+++ lab2-nodejs-broken-autentication/application/app.js
@@ -12,6 +12,17 @@ var jwt = require('jsonwebtoken');
 const fs   = require('fs');
 const { randomUUID } = require('crypto');
 
+var RateLimit = require('express-rate-limit');
+
+var limiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes 
+    max: 50, // limit each IP to 100 requests per windowMs 
+    delayMs: 0, // disable delaying - full speed until the max limit is reached 
+    message: "Too many accounts created from this IP, please try again after an hour"
+  });
+
+app.use(limiter);
+
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
 app.use(cookieParser()); 
--- lab2-nodejs-broken-autentication/black-hat-env/brutal-force/brute.sh
+++ lab2-nodejs-broken-autentication/black-hat-env/brutal-force/brute.sh
@@ -7,9 +7,11 @@ for user in $(cat $USERNAMES_FILE); do
     for pass in $(cat $PASSWORDS_FILE); do
         RESPONSE=$(curl -s -XPOST localhost:3000/login --header 'Content-Type:application/json' -d '{"username": "'$user'","password": "'$pass'"}')
 
-        if [[ $RESPONSE == *"token"* ]]; then
-            echo "User:" $user " - Pass:" $pass " - Token: " $RESPONSE
-            break;
-        fi
+        echo $RESPONSE
+
+        #if [[ $RESPONSE == *"token"* ]]; then
+        #    echo "User:" $user " - Pass:" $pass " - Token: " $RESPONSE
+        #    break;
+        #fi
     done
 done
